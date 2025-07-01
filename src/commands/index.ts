157528d98646c7e8e092468dfff8c102dfc92494
Samuel <samuel.oosterholt20001004@gmail.com>
2025-07-01 23:46:00 +0200

    ci: fix Dockerfile and release

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -29,6 +29,19 @@ export class Commands {
       name: 'version',
     });
 
+    this.register(
+      'uptime',
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async (ctx, args) => {
+        await reply(ctx, `[🕓] Uptime: ${Math.round(process.uptime())} seconds`);
+      },
+      {
+        description: 'Show uptime',
+        args: [],
+        name: 'uptime',
+      },
+    );
+
     this.register('help', help(this.commands), {
       name: 'help',
       args: [{ name: 'command', required: false, description: 'Command to get detailed help for' }],
